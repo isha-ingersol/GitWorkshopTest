fb6d5ee7a1c922bfd991a445b1376dab8e614ac5
isha-ingersol <isha.ingersol@gmail.com>
2023-02-09 16:18:21 +0400

    Wrote some more more code

--- src/GitWorkshopTest.java
+++ src/GitWorkshopTest.java
@@ -2,6 +2,7 @@ public class GitWorkshopTest
 {
     public static void main(String[] args) throws Exception
     {
+        System.out.println("This is me writing more more code!");
         System.out.println("Hello World from the Visual Studio Code IDE!");
         System.out.println("This is me writing more code!");
     }
